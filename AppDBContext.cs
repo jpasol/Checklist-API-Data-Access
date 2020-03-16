1436310e24b842b09b3d4d45b069e37710433beb
Jaspher Jan Pasol <jpasol@sbitc.ph>
2020-03-16 10:17:02 +0800

    Alpha v1.0.0

---
+++ AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using EquipmentChecklistDataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EquipmentChecklistDataAccess
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Components> Components { get; set; }
+        public DbSet<Issues> Issues { get; set; }
+    }
+}
