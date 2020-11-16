c3e496c369bec11e7fbdf32b6871a637a4711a41
jpasol <38642594+jpasol@users.noreply.github.com>
2020-11-16 17:40:45 +0800

    added migrations

---
+++ Migrations/20201116081102_AddedQuestionRelationships.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EquipmentChecklistDataAccess.Migrations
+{
+    public partial class AddedQuestionRelationships : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Checklist_Items_Equipment_TypeID",
+                table: "Checklist_Items");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Checklist_Items_Equipment_TypeID_ComponentID",
+                table: "Checklist_Items",
+                columns: new[] { "Equipment_TypeID", "ComponentID" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Checklist_Items_Questions_Equipment_TypeID_ComponentID",
+                table: "Checklist_Items",
+                columns: new[] { "Equipment_TypeID", "ComponentID" },
+                principalTable: "Questions",
+                principalColumns: new[] { "Equipment_TypeID", "ComponentID" },
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Checklist_Items_Questions_Equipment_TypeID_ComponentID",
+                table: "Checklist_Items");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Checklist_Items_Equipment_TypeID_ComponentID",
+                table: "Checklist_Items");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Checklist_Items_Equipment_TypeID",
+                table: "Checklist_Items",
+                column: "Equipment_TypeID");
+        }
+    }
+}
