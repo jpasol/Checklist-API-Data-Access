739d1ea44a2a9b40f6e2426c2acf7a6ae7f2a677
jpasol <38642594+jpasol@users.noreply.github.com>
2021-02-09 10:21:08 +0800

    Create Voucher.cs

---
+++ Models/Voucher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+
+namespace EquipmentChecklistDataAccess.Models
+{
+    class Voucher
+    {
+        [ForeignKey("UserID")]
+        [Column(TypeName = "NVARCHAR(15)")]
+        public User User { get; set; }
+        public string UserID { get; set; }
+
+        [ForeignKey("EquipmentID")]
+        [Column(TypeName = "NVARCHAR(5)")]
+        public Equipment Equipment { get; set; }
+        public string EquipmentID { get; set; }
+
+        public DateTime Validity { get; set; }
+    }
+
+}
