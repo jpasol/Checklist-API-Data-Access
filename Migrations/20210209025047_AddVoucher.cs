37c559ab850370fa98f1f00f9364452c9513eeee
jpasol <38642594+jpasol@users.noreply.github.com>
2021-02-09 11:27:48 +0800

    added migration

---
+++ Migrations/20210209025047_AddVoucher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EquipmentChecklistDataAccess.Migrations
+{
+    public partial class AddVoucher : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vouchers",
+                columns: table => new
+                {
+                    UserID = table.Column<string>(type: "NVARCHAR(15)", nullable: false),
+                    EquipmentID = table.Column<string>(type: "NVARCHAR(5)", nullable: false),
+                    Validity = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vouchers", x => new { x.UserID, x.EquipmentID });
+                    table.ForeignKey(
+                        name: "FK_Vouchers_Equipments_EquipmentID",
+                        column: x => x.EquipmentID,
+                        principalTable: "Equipments",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Vouchers_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vouchers_EquipmentID",
+                table: "Vouchers",
+                column: "EquipmentID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vouchers");
+        }
+    }
+}
